4482cafa1215f417c15712599afd31d4fb7a4570
NoChanceSD <nochance20@gmail.com>
2015-08-24 14:05:16 +0100

    Update to 1.8.3+

---
+++ src/main/java/org/mcteam/ancientgates/tasks/BungeeServerList.java
@@ -0,0 +1,26 @@
+package org.mcteam.ancientgates.tasks;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.mcteam.ancientgates.Plugin;
+import org.mcteam.ancientgates.util.types.PluginMessage;
+
+import com.google.common.collect.Iterables;
+
+public class BungeeServerList extends BukkitRunnable {
+ 
+	private final Plugin plugin;
+ 
+	public BungeeServerList(Plugin plugin) {
+		this.plugin = plugin;
+	}
+ 
+	public void run() {
+		if (plugin.getServer().getOnlinePlayers().size() == 0) return;
+		
+		// Send BungeeCord "GetServers" command
+		final PluginMessage msg = new PluginMessage("GetServers");
+		Iterables.getFirst(Bukkit.getOnlinePlayers(), null).sendPluginMessage(plugin, "BungeeCord", msg.toByteArray());
+	}
+	
+}
---
+++ src/main/java/org/mcteam/ancientgates/tasks/BungeeServerName.java
@@ -0,0 +1,30 @@
+package org.mcteam.ancientgates.tasks;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.mcteam.ancientgates.Plugin;
+import org.mcteam.ancientgates.util.types.PluginMessage;
+
+import com.google.common.collect.Iterables;
+
+public class BungeeServerName extends BukkitRunnable {
+ 
+	private final Plugin plugin;
+ 
+	public BungeeServerName(Plugin plugin) {
+		this.plugin = plugin;
+	}
+ 
+	public void run() {
+		if (Plugin.bungeeServerName != null) return;
+		if (plugin.getServer().getOnlinePlayers().size() == 0) return;
+		
+		// Send BungeeCord "GetServer" command
+		final PluginMessage msg = new PluginMessage("GetServer");
+		Iterables.getFirst(Bukkit.getOnlinePlayers(), null).sendPluginMessage(plugin, "BungeeCord", msg.toByteArray());
+		
+		// Re-schedule task to check bungeeServerName set
+		if (Plugin.bungeeServerName == null) new BungeeServerName(plugin).runTaskLater(plugin, 20L);
+	}
+	
+}
