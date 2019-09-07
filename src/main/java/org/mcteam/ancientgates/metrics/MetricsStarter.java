d81da289d2a240ed8ca8591ced0feb8f1d96756f
NoChanceSD <nochance20@gmail.com>
2019-09-07 07:05:04 +0100

    Switch to bstats, mcstats is dead

--- src/main/java/org/mcteam/ancientgates/metrics/MetricsStarter.java
+++ src/main/java/org/mcteam/ancientgates/metrics/MetricsStarter.java
@@ -1,13 +1,13 @@
 package org.mcteam.ancientgates.metrics;
 
-import java.io.IOException;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Material;
 import org.mcteam.ancientgates.Conf;
 import org.mcteam.ancientgates.Gate;
 import org.mcteam.ancientgates.Plugin;
-import org.mcstats.Metrics;
-import org.mcstats.Metrics.Graph;
 
 public class MetricsStarter {
 
@@ -18,238 +18,95 @@ public class MetricsStarter {
     }
 
     public void setupMetrics() {
-
-        try {
             // Initialise metics
             final Metrics metrics = new Metrics(plugin);
+            
+            // It seems bstats doesn't support multi line or bar charts currently, leaving this here for when it does
+//			metrics.addCustomChart(new Metrics.MultiLineChart("number_of_gates", new Callable<Map<String, Integer>>() {
+//				@Override
+//				public Map<String, Integer> call() throws Exception {
+//					Map<String, Integer> valueMap = new HashMap<>();
+//					valueMap.put("Bungee Gates", (int) Gate.getAll().stream().filter(g -> g.getBungeeTos() != null).count());
+//					valueMap.put("Normal Gates", (int) Gate.getAll().stream().filter(g -> g.getTos() != null).count());
+//					valueMap.put("Undefined Gates", (int) Gate.getAll().stream().filter(g -> g.getTos() == null).count());
+//					valueMap.put("Total", Gate.getAll().size());
+//					return valueMap;
+//				}
+//			}));
 
-            // Plot number of gates
-            final Graph gatesGraph = metrics.createGraph("Number of Gates");
-            gatesGraph.addPlotter(new Metrics.Plotter("Bungee Gates") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getBungeeTos() != null)
-                            i++;
-                    }
-                    return i;
-                }
-            });
-            gatesGraph.addPlotter(new Metrics.Plotter("Normal Gates") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getTos() != null)
-                            i++;
-                    }
-                    return i;
-                }
-            });
-            gatesGraph.addPlotter(new Metrics.Plotter("Undefined Gates") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getTos() == null)
-                            i++;
-                    }
-                    return i;
-                }
-            });
-            gatesGraph.addPlotter(new Metrics.Plotter("Total") {
-                @Override
-                public int getValue() {
-                    return Gate.getAll().size();
-                }
-            });
+			// Plot number of gates
+			metrics.addCustomChart(new Metrics.AdvancedPie("number_of_gates", new Callable<Map<String, Integer>>() {
+				@Override
+				public Map<String, Integer> call() throws Exception {
+					Map<String, Integer> valueMap = new HashMap<>();
+					valueMap.put("Bungee Gates", (int) Gate.getAll().stream().filter(g -> g.getBungeeTos() != null).count());
+					valueMap.put("Normal Gates", (int) Gate.getAll().stream().filter(g -> g.getTos() != null).count());
+					valueMap.put("Undefined Gates", (int) Gate.getAll().stream().filter(g -> g.getTos() == null).count());
+					return valueMap;
+				}
+			}));
 
             // Plot gate entity access
-            final Graph accessGraph = metrics.createGraph("Gate Access");
-            accessGraph.addPlotter(new Metrics.Plotter("Players") {
-                @Override
-                public int getValue() {
-                    return Gate.getAll().size();
-                }
-            });
-            accessGraph.addPlotter(new Metrics.Plotter("Entities") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getTeleportEntities())
-                            i++;
-                    }
-                    return i;
-                }
-            });
-            accessGraph.addPlotter(new Metrics.Plotter("Vehicles") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getTeleportVehicles())
-                            i++;
-                    }
-                    return i;
-                }
-            });
-            gatesGraph.addPlotter(new Metrics.Plotter("Total") {
-                @Override
-                public int getValue() {
-                    return Gate.getAll().size();
-                }
-            });
+			metrics.addCustomChart(new Metrics.AdvancedPie("gate_access", new Callable<Map<String, Integer>>() {
+				@Override
+				public Map<String, Integer> call() throws Exception {
+					Map<String, Integer> valueMap = new HashMap<>();
+					valueMap.put("Players", Gate.getAll().size());
+					valueMap.put("Entities", (int) Gate.getAll().stream().filter(g -> g.getTeleportEntities()).count());
+					valueMap.put("Vehicles", (int) Gate.getAll().stream().filter(g -> g.getTeleportVehicles()).count());
+					return valueMap;
+				}
+			}));
 
             // Plot number of servers
-            final Graph serverGraph = metrics.createGraph("Number of Servers");
-            serverGraph.addPlotter(new Metrics.Plotter("Bungee Servers") {
-                @Override
-                public int getValue() {
-                    return Conf.bungeeCordSupport ? 1 : 0;
-                }
-            });
-            serverGraph.addPlotter(new Metrics.Plotter("Normal Servers") {
-                @Override
-                public int getValue() {
-                    return Conf.bungeeCordSupport ? 0 : 1;
-                }
-            });
-            serverGraph.addPlotter(new Metrics.Plotter("Total") {
-                @Override
-                public int getValue() {
-                    return 1;
-                }
-            });
+			metrics.addCustomChart(new Metrics.SimplePie("number_of_servers", new Callable<String>() {
+				@Override
+				public String call() throws Exception {
+					return Conf.bungeeCordSupport ? "Bungee Servers" : "Normal Servers";
+				}
+			}));
 
             // Plot features
-            final Graph featureGraph = metrics.createGraph("Features");
-            featureGraph.addPlotter(new Metrics.Plotter("BungeeCord Support") {
-                @Override
-                public int getValue() {
-                    return Conf.bungeeCordSupport ? 1 : 0;
-                }
-            });
-            featureGraph.addPlotter(new Metrics.Plotter("Socket Comms Enabled") {
-                @Override
-                public int getValue() {
-                    return Conf.useSocketComms ? 1 : 0;
-                }
-            });
-            featureGraph.addPlotter(new Metrics.Plotter("Economy Enabled") {
-                @Override
-                public int getValue() {
-                    return Conf.useEconomy ? 1 : 0;
-                }
-            });
-            featureGraph.addPlotter(new Metrics.Plotter("Enforce Access Enabled") {
-                @Override
-                public int getValue() {
-                    return Conf.enforceAccess ? 1 : 0;
-                }
-            });
+			metrics.addCustomChart(new Metrics.AdvancedPie("features", new Callable<Map<String, Integer>>() {
+				@Override
+				public Map<String, Integer> call() throws Exception {
+					Map<String, Integer> valueMap = new HashMap<>();
+					valueMap.put("BungeeCord Support", Conf.bungeeCordSupport ? 1 : 0);
+					valueMap.put("Socket Comms Enabled", Conf.useSocketComms ? 1 : 0);
+					valueMap.put("Economy Enabled", Conf.useEconomy ? 1 : 0);
+					valueMap.put("Enforce Access Enabled", Conf.enforceAccess ? 1 : 0);
+					return valueMap;
+				}
+			}));
 
             // Plot teleportation method
-            final Graph methodGraph = metrics.createGraph("Teleportation method");
-            methodGraph.addPlotter(new Metrics.Plotter("Movement Hook") {
-                @Override
-                public int getValue() {
-                    return Conf.useVanillaPortals ? 0 : 1;
-                }
-            });
-            methodGraph.addPlotter(new Metrics.Plotter("Vanilla Portal") {
-                @Override
-                public int getValue() {
-                    return Conf.useVanillaPortals ? 1 : 0;
-                }
-            });
+			metrics.addCustomChart(new Metrics.SimplePie("teleportation_method", new Callable<String>() {
+				@Override
+				public String call() throws Exception {
+					return Conf.useVanillaPortals ? "Vanilla Portal" : "Movement Hook";
+				}
+			}));
 
             // Plot gate portal materials
-            final Graph materialsGraph = metrics.createGraph("Gate Materials");
-            materialsGraph.addPlotter(new Metrics.Plotter("Web") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getMaterial() == Material.WEB)
-                            i++;
-                    }
-                    return i;
-                }
-            });
-            materialsGraph.addPlotter(new Metrics.Plotter("Water") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getMaterial() == Material.STATIONARY_WATER)
-                            i++;
-                    }
-                    return i;
-                }
-            });
-            materialsGraph.addPlotter(new Metrics.Plotter("Sugar Cane") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getMaterial() == Material.SUGAR_CANE_BLOCK)
-                            i++;
-                    }
-                    return i;
-                }
-            });
-            materialsGraph.addPlotter(new Metrics.Plotter("Portal") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getMaterial() == Material.PORTAL)
-                            i++;
-                    }
-                    return i;
-                }
-            });
-            materialsGraph.addPlotter(new Metrics.Plotter("Lava") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getMaterial() == Material.STATIONARY_LAVA)
-                            i++;
-                    }
-                    return i;
-                }
-            });
-            materialsGraph.addPlotter(new Metrics.Plotter("End Portal") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getMaterial() == Material.ENDER_PORTAL)
-                            i++;
-                    }
-                    return i;
-                }
-            });
-            materialsGraph.addPlotter(new Metrics.Plotter("Air") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Gate gate : Gate.getAll()) {
-                        if (gate.getMaterial() == Material.PISTON_MOVING_PIECE)
-                            i++;
-                    }
-                    return i;
-                }
-            });
+			metrics.addCustomChart(new Metrics.AdvancedPie("gate_materials", new Callable<Map<String, Integer>>() {
+				@Override
+				public Map<String, Integer> call() throws Exception {
+					Map<String, Integer> valueMap = new HashMap<>();
+					valueMap.put("Web", getGateCountByMaterial(Material.WEB));
+					valueMap.put("Water", getGateCountByMaterial(Material.STATIONARY_WATER));
+					valueMap.put("Sugar Cane", getGateCountByMaterial(Material.SUGAR_CANE_BLOCK));
+					valueMap.put("Portal", getGateCountByMaterial(Material.PORTAL));
+					valueMap.put("Lava", getGateCountByMaterial(Material.STATIONARY_LAVA));
+					valueMap.put("End Portal", getGateCountByMaterial(Material.ENDER_PORTAL));
+					valueMap.put("Air", getGateCountByMaterial(Material.PISTON_MOVING_PIECE));
+					return valueMap;
+				}
 
-            // Submit metrics
-            metrics.start();
-        } catch (final IOException e) {
-            // Failed to submit the stats :-(
-        }
+				private int getGateCountByMaterial(Material material) {
+					return (int) Gate.getAll().stream().filter(g -> g.getMaterial() == material).count();
+				}
+			}));
+
     }
 
 }
